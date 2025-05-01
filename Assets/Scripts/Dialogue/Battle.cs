86fb958000f8ba4f80b5ddaff06c8dd8cbda8acd
Rachel <1187859081@qq.com>
2025-05-02 00:11:10 +0800

    Add files via upload

--- Assets/Scripts/Dialogue/Battle.cs
+++ Assets/Scripts/Dialogue/Battle.cs
@@ -12,10 +12,13 @@ public class Battle : MonoBehaviour
     private void Awake()
     {
         var dialogTexts = new List<DialogData>();
+        var dialogTexts_en = new List<DialogData>();
 
         dialogTexts.Add(new DialogData("在探索的同时，你也可能被隐藏的怪物发现而被拉入到奇异的空间中", "神秘人"));
         dialogTexts.Add(new DialogData("根据提示通过战斗击败怪物，或者解开谜题，方能离开", "神秘人"));
 
+        dialogTexts_en.Add(new DialogData("While exploring, you may be discovered by hidden monsters and pulled into a strange space.", "Mystery Man"));
+        dialogTexts_en.Add(new DialogData("Defeat the monsters in battle or solve the puzzles as instructed to proceed.", "Mystery Man"));
         DialogManager.Show(dialogTexts);
 
     }
